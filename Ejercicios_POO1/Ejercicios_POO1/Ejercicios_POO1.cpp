75ea78cc805a53adff3c5254d69724bb489e023e
Skinde <46155561+Skinde@users.noreply.github.com>
2019-10-04 11:42:25 -0500

    Agregado CMake

---
+++ Ejercicios_POO1/Ejercicios_POO1/Ejercicios_POO1.cpp
@@ -0,0 +1,12 @@
+﻿// Ejercicios_POO1.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "Ejercicios_POO1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hola CMake." << endl;
+	return 0;
+}
